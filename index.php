8511166cbdd42a96d8295344be97388001e37e91
Herman Wijaya <45072562+0xh1n0k1@users.noreply.github.com>
2023-01-08 17:00:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>This website has been hacked</title>
 </head>
 <body>
-    <h1>This website has been defaced. </h1>
+    <h1>This website has been defaced.</h1>
     <h2>~h1n0k1</h2>
 </body>
 </html>
